4aa0de69ebaa0af5b2e6dc7384aff1d465c6c23d
mreshtaiwi <mr.eshtaiwi@gmail.com>
2021-04-25 14:10:12 +0300

    html css js demo

---
+++ class-01/lab-b/js/app.js
@@ -0,0 +1,33 @@
+'use strict';
+// var userName = prompt('please enter you name?');
+
+// console.log(i);
+var i = 35;
+var i = 20;
+console.log('i = ', i);
+
+
+
+let age = 35;
+// let age = 40; i can't redeclare
+age = 40;// i can change the value
+console.log('age = ', age);
+
+
+
+
+// console.log('salary = ', salary);
+let salary = 2000;
+
+
+
+// console.log('bi = ', bi);
+const bi = 3.14;
+// bi= 4; this is wrong i can't reassign const variable
+console.log('bi = ', bi);
+let sum = 0;
+let num1 = parseInt(prompt('please enter number 1?'));//20
+let num2 = parseInt(prompt('please enter number 2'));//20
+
+sum = num1 + num2; //2020
+console.log(sum);
